4842f6f259d4e9c7f14a177e332c2e7a5956be77
Sarthak rote <sarthakrote00@gmail.com>
2025-05-31 14:05:54 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -42,16 +42,18 @@ public class bubblesort {
     static int bubbleSort(int [] arr) {
               for(int i=0; i<arr.length; i++) {
                       for(int j=1; j<arr.length-i; j++) {
-                                      if(arr[j]<arr[j-1]) {
+                       
+                  if(arr[j]<arr[j-1]) {
 
-                                            int temp=arr[j];
+                                               int temp=arr[j];
                                             arr[j]=arr[j-1];
                                             arr[j-1]=temp;
+
 }
                 
              }
        }
-//         return -1;
+  return -1;
     
 
 
